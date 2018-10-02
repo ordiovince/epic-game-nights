6c639deac194d494eb30f56a5de0224a359529b6
Vincent Ordioni <vordioni@smart-tale.com>
2018-10-02 18:17:16 +0200

    Edit and delete nights

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,6 +6,8 @@
         <b-card-group deck class="game-list">
           <div class="card game" v-for="(night, index) in nights" :key="index">
             <div class="card-body">
+              <b-button class="float-left" v-b-modal.deleteNight @click="selectedNight = night"><font-awesome-icon icon="trash" /></b-button>
+              <b-button class="float-right" v-b-modal.editNight @click="selectedNight = night"><font-awesome-icon icon="edit" /></b-button>
               <h5 class="card-title">{{ night.date.toDate() | formatDate }}</h5>
               <p class="card-text">Nombre de joueurs prévus : {{ night.numberOfPlayers }}</p>
             </div>
@@ -43,6 +45,37 @@
         </b-tab>
       </b-tabs>
     </section>
+
+    <!-- Modal Component -->
+    <b-modal id="deleteNight" hide-header lazy @ok="deleteNight">
+      <div v-if="selectedNight">
+        Veux-tu vraiment supprimer cette Game Night du {{ selectedNight.date.toDate() | formatDate }} ?
+      </div>
+    </b-modal>
+    <b-modal id="editNight" hide-header lazy @ok="editNight">
+      <div v-if="selectedNight">
+        <b-form-group id="dateGroup"
+                      label="Date"
+                      label-for="date">
+          <b-form-input id="date"
+                        type="date"
+                        required
+                        v-model="selectedNight.dateFormated"
+                        placeholder="01/01/2018">
+          </b-form-input>
+        </b-form-group>
+        <b-form-group id="numberOfPlayersGroup"
+                      label="Nombre de joueurs prévus"
+                      label-for="numberOfPlayers">
+          <b-form-input id="numberOfPlayers"
+                        type="number"
+                        required
+                        v-model="selectedNight.numberOfPlayers"
+                        placeholder="0">
+          </b-form-input>
+        </b-form-group>
+      </div>
+    </b-modal>
   </div>
 </template>
 
@@ -76,6 +109,7 @@ export default {
         { key: 'averagePlayTime', label: "Temps moyen de jeu", sortable: true },
         { key: 'link', label: "Lien", sortable: false }
       ],
+      selectedNight: null
     };
   },
   created () {
@@ -89,6 +123,7 @@ export default {
   },
   methods: {
     fetchData () {
+      this.games = [];
       Firebase.db.collection("games").orderBy("name").get().then((querySnapshot) => {
         for (let i = 0; i < querySnapshot.docs.length; i++) {
           let game = querySnapshot.docs[i].data();
@@ -101,24 +136,38 @@ export default {
         });
       });
 
+      this.nights = [];
       Firebase.db.collection("nights").where("date", ">=", new Date()).get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
           let night = doc.data();
           night.id = doc.id;
+          night.dateFormated = new Moment(String(night.date.toDate()), "ddd MMM DD YYYY HH:mm:ss ZZ").format('YYYY-MM-DD');
           this.nights.push(night);
-          /*
-          for (let i = 0; i < night.votes.length; i++) {
-            let vote = night.votes[i];
-            for (let j = 0; j < vote.scores.length; j++) {
-              let score = vote.scores[j];
-              score.game.get().then((game) => {
-                console.log(game.data());
-              });
-            }
-          }
-          */
-  });
-});
+        });
+      });
+    },
+    deleteNight () {
+      Firebase.db.collection("nights").doc(this.selectedNight.id).delete()
+      .then(() => {
+        console.log("Document successfully deleted!");
+        this.fetchData();
+      }).catch((error) => {
+        console.error("Error removing document: ", error);
+      });
+    },
+    editNight () {
+      Firebase.db.collection("nights").doc(this.selectedNight.id).update({
+        date: new Date(this.selectedNight.dateFormated),
+        numberOfPlayers: Number(this.selectedNight.numberOfPlayers)
+      })
+      .then(() => {
+        console.log("Document successfully edited");
+        this.fetchData();
+      })
+      .catch((error) => {
+        console.error("Error updating document: ", error);
+        alert("Error updating document: ".error);
+      });
     }
   }
 }
